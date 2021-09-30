a3e631dd9464ccedb0e4fc356ac158bd70d42a6e
todd reid <notdelltaco@hotmail.com>
2021-09-29 21:00:11 -0400

    started javascript

---
+++ assets/js/script.js
@@ -0,0 +1,139 @@
+
+
+var now = new Date();
+console.log(now);
+
+//fetch('http://worldtimeapi.org/api/timezone/Europe/London')
+//  .then(response => {
+//    return response.json();
+// })
+//.then(users => {
+//   console.log(users);
+// });
+
+//fetch('http://worldtimeapi.org/api/timezone/Europe/Berlin')
+//  .then(response => {
+//    return response.json();
+//})
+//.then(users => {
+// console.log(users);
+//});
+
+//fetch('http://worldtimeapi.org/api/timezone/Europe/Moscow')
+//.then(response => {
+//   return response.json();
+//})
+// .then(users => {
+//      console.log(users);
+//   });
+
+//fetch('http://worldtimeapi.org/api/timezone/Europe/Bucharest')
+//.then(response => {
+//   return response.json();
+//})
+//.then(users => {
+//   console.log(users);
+// });
+
+//  fetch('http://worldtimeapi.org/api/timezone/America/New_York')
+//.then(response => {
+//   return response.json();
+//})
+//.then(users => {
+//  console.log(users);
+//});
+
+//fetch('http://worldtimeapi.org/api/timezone/America/Chicago')
+//.then(response => {
+//  return response.json();
+//})
+//.then(users => {
+//   console.log(users);
+//});
+
+//fetch('http://worldtimeapi.org/api/timezone/America/Boise')
+//.then(response => {
+//   return response.json();
+//})
+//.then(users => {
+//  console.log(users);
+//});
+
+//fetch('http://worldtimeapi.org/api/timezone/America/Los_Angeles')
+//.then(response => {
+//  return response.json();
+//})
+//.then(users => {
+//   console.log(users);
+// });
+
+// fetch('http://worldtimeapi.org/api/timezone/America/New_York')
+//.then(response => {
+// return response.json();
+//})
+//.then(users => {
+//  console.log(users);
+//});
+fetch('http://worldtimeapi.org/api/timezone/Asia')
+ .then(response => {
+     return response.json();
+ })
+ .then(users => {
+     console.log(users);
+ });
+
+fetch('http://worldtimeapi.org/api/timezone/Asia/Dubai')
+    .then(response => {
+        return response.json();
+    })
+    .then(users => {
+        console.log(users);
+    });
+
+    fetch('http://worldtimeapi.org/api/timezone/Asia/Hong_Kong')
+    .then(response => {
+        return response.json();
+    })
+    .then(users => {
+        console.log(users);
+    });
+
+    fetch('http://worldtimeapi.org/api/timezone/Asia/Tokyo')
+ .then(response => {
+     return response.json();
+ })
+ .then(users => {
+     console.log(users);
+ });
+
+ fetch('http://worldtimeapi.org/api/timezone/Asia/Mumbai')
+ .then(response => {
+     return response.json();
+ })
+ .then(users => {
+     console.log(users);
+ });
+
+ fetch('http://worldtimeapi.org/api/timezone/Australia/Sydney')
+ .then(response => {
+     return response.json();
+ })
+ .then(users => {
+     console.log(users);
+ });
+
+ fetch('http://worldtimeapi.org/api/timezone/Australia/Auckland')
+ .then(response => {
+     return response.json();
+ })
+ .then(users => {
+     console.log(users);
+ });
+
+ fetch('http://worldtimeapi.org/api/timezone/South_America')
+ .then(response => {
+     return response.json();
+ })
+ .then(users => {
+     console.log(users);
+ });
