b437d24cf36b27562aa7fff20a94cdabc433bf4c
todd reid <notdelltaco@hotmail.com>
2021-10-05 19:28:24 -0400

    fetch showing on html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,189 +1,41 @@
+(async () => {
+    const now = new Date();
+    const data = await
+    Promise.all([
+    fetch('http://worldtimeapi.org/api/timezone/Europe/London').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/Europe/Berlin').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/Europe/Moscow').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/Europe/Bucharest').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/America/New_York').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/America/Chicago').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/America/Boise').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/America/Los_Angeles').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/Asia/Dubai').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/Asia/Hong_Kong').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/Asia/Tokyo').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/Asia/Mumbai').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/Australia/Sydney').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/Australia/Darwin').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/America/Argentina/Buenos_Aires').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/America/Lima').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/America/Puerto_Rico').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/Africa').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/Africa/Algiers').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/Africa/Cairo').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/Africa/Juba').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/Africa/Bissau').then(response => response.json()),
+]);
 
+for (const time of data) {
+    const div = document.createElement("div");
+    div.textContent = time.datetime;
+    document.body.appendChild(div);
+}
+})();
 
-var now = new Date();
-console.log(now);
-
-//fetch('http://worldtimeapi.org/api/timezone/Europe/London')
-//  .then(response => {
-//    return response.json();
-// })
-//.then(users => {
-//   console.log(users);
-// });
-
-//fetch('http://worldtimeapi.org/api/timezone/Europe/Berlin')
-//  .then(response => {
-//    return response.json();
-//})
-//.then(users => {
-// console.log(users);
-//});
-
-//fetch('http://worldtimeapi.org/api/timezone/Europe/Moscow')
-//.then(response => {
-//   return response.json();
-//})
-// .then(users => {
-//      console.log(users);
-//   });
-
-//fetch('http://worldtimeapi.org/api/timezone/Europe/Bucharest')
-//.then(response => {
-//   return response.json();
-//})
-//.then(users => {
-//   console.log(users);
-// });
-
-//  fetch('http://worldtimeapi.org/api/timezone/America/New_York')
-//.then(response => {
-//   return response.json();
-//})
-//.then(users => {
-//  console.log(users);
-//});
-
-//fetch('http://worldtimeapi.org/api/timezone/America/Chicago')
-//.then(response => {
-//  return response.json();
-//})
-//.then(users => {
-//   console.log(users);
-//});
-
-//fetch('http://worldtimeapi.org/api/timezone/America/Boise')
-//.then(response => {
-//   return response.json();
-//})
-//.then(users => {
-//  console.log(users);
-//});
-
-//fetch('http://worldtimeapi.org/api/timezone/America/Los_Angeles')
-//.then(response => {
-//  return response.json();
-//})
-//.then(users => {
-//   console.log(users);
-// });
-
-// fetch('http://worldtimeapi.org/api/timezone/America/New_York')
-//.then(response => {
-// return response.json();
-//})
-//.then(users => {
-//  console.log(users);
-//});
-
-//fetch('http://worldtimeapi.org/api/timezone/Asia/Dubai')
-//  .then(response => {
-//    return response.json();
-//})
-//.then(users => {
-//  console.log(users);
-//});
-
-//fetch('http://worldtimeapi.org/api/timezone/Asia/Hong_Kong')
-//.then(response => {
-//  return response.json();
-//})
-//.then(users => {
-//   console.log(users);
-//});
-
-//    fetch('http://worldtimeapi.org/api/timezone/Asia/Tokyo')
-//.then(response => {
-//  return response.json();
-//})
-//.then(users => {
-//   console.log(users);
-//});
-
-//fetch('http://worldtimeapi.org/api/timezone/Asia/Mumbai')
-//.then(response => {
-//  return response.json();
-//})
-//.then(users => {
-//   console.log(users);
-//});
-
-//fetch('http://worldtimeapi.org/api/timezone/Australia/Sydney')
-    //.then(response => {
-      //  return response.json();
-    //})
-   // .then(users => {
-    //    console.log(users);
-  //  });
-
-//fetch('http://worldtimeapi.org/api/timezone/Australia/Darwin')
-    //.then(response => {
-      //  return response.json();
-    //})
-    //.then(users => {
-      //  console.log(users);
-    //});
-
-    //fetch('http://worldtimeapi.org/api/timezone/America/Argentina/Buenos_Aires')
-    //.then(response => {
-     //   return response.json();
-   // })
-    //.then(users => {
-     //   console.log(users);
-   // });
-
-   // fetch('http://worldtimeapi.org/api/timezone/America/Lima')
-    //.then(response => {
-     //   return response.json();
-   // })
-   // .then(users => {
-  //      console.log(users);
-//    });
-
-    //fetch('http://worldtimeapi.org/api/timezone/America/Puerto_Rico')
-    //.then(response => {
-     //   return response.json();
-   // })
-    //.then(users => {
-      //  console.log(users);
-    //});
-
-   //fetch('http://worldtimeapi.org/api/timezone/Africa')
-    //.then(response => {
-      //  return response.json();
-    //})
-    //.then(users => {
-      //  console.log(users);
-    //});
-
-    //fetch('http://worldtimeapi.org/api/timezone/Africa/Algiers')
-    //.then(response => {
-      //  return response.json();
-    //})
-    //.then(users => {
-      //  console.log(users);
-    //});
-
-    //fetch('http://worldtimeapi.org/api/timezone/Africa/Cairo')
-    //.then(response => {
-      //  return response.json();
-    //})
-    //.then(users => {
-      //  console.log(users);
-    //});
-
-    //fetch('http://worldtimeapi.org/api/timezone/Africa/Juba')
-    //.then(response => {
-     //   return response.json();
-    //})
-    //.then(users => {
-  //      console.log(users);
-//    });
-
-    //fetch('http://worldtimeapi.org/api/timezone/Africa/Bissau')
-    //.then(response => {
-      //  return response.json();
-    //})
-    //.then(users => {
-    //    console.log(users);
-  //  });
-
+const eu = [
+    fetch('http://worldtimeapi.org/api/timezone/Europe/London').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/Europe/Berlin').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/Europe/Moscow').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/Europe/Bucharest').then(response => response.json()),
+]
