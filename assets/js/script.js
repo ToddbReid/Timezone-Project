c1d86c7cd546f2ba470f09814445d5e3bdcf9dde
todd reid <notdelltaco@hotmail.com>
2021-10-06 18:10:06 -0400

    arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,19 +13,19 @@
     fetch('http://worldtimeapi.org/api/timezone/Asia/Dubai').then(response => response.json()),
     fetch('http://worldtimeapi.org/api/timezone/Asia/Hong_Kong').then(response => response.json()),
     fetch('http://worldtimeapi.org/api/timezone/Asia/Tokyo').then(response => response.json()),
-    fetch('http://worldtimeapi.org/api/timezone/Asia/Mumbai').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/Asia/Kolkata').then(response => response.json()),
     fetch('http://worldtimeapi.org/api/timezone/Australia/Sydney').then(response => response.json()),
     fetch('http://worldtimeapi.org/api/timezone/Australia/Darwin').then(response => response.json()),
     fetch('http://worldtimeapi.org/api/timezone/America/Argentina/Buenos_Aires').then(response => response.json()),
     fetch('http://worldtimeapi.org/api/timezone/America/Lima').then(response => response.json()),
     fetch('http://worldtimeapi.org/api/timezone/America/Puerto_Rico').then(response => response.json()),
-    fetch('http://worldtimeapi.org/api/timezone/Africa').then(response => response.json()),
     fetch('http://worldtimeapi.org/api/timezone/Africa/Algiers').then(response => response.json()),
     fetch('http://worldtimeapi.org/api/timezone/Africa/Cairo').then(response => response.json()),
     fetch('http://worldtimeapi.org/api/timezone/Africa/Juba').then(response => response.json()),
     fetch('http://worldtimeapi.org/api/timezone/Africa/Bissau').then(response => response.json()),
+    
 ]);
-
+console.log(fetch('http://worldtimeapi.org/api/timezone/Asia/Kolkata').then(response => response.json()));
 for (const time of data) {
     const div = document.createElement("div");
     div.textContent = time.datetime;
@@ -38,4 +38,36 @@ const eu = [
     fetch('http://worldtimeapi.org/api/timezone/Europe/Berlin').then(response => response.json()),
     fetch('http://worldtimeapi.org/api/timezone/Europe/Moscow').then(response => response.json()),
     fetch('http://worldtimeapi.org/api/timezone/Europe/Bucharest').then(response => response.json()),
+]
+
+const na = [
+    fetch('http://worldtimeapi.org/api/timezone/America/New_York').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/America/Chicago').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/America/Boise').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/America/Los_Angeles').then(response => response.json()),
+]
+
+const as = [
+    fetch('http://worldtimeapi.org/api/timezone/Asia/Dubai').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/Asia/Hong_Kong').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/Asia/Tokyo').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/Asia/Kolkata').then(response => response.json()),
+]
+
+const aus = [
+    fetch('http://worldtimeapi.org/api/timezone/Australia/Sydney').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/Australia/Darwin').then(response => response.json()),
+]
+
+const sa = [
+    fetch('http://worldtimeapi.org/api/timezone/America/Argentina/Buenos_Aires').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/America/Lima').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/America/Puerto_Rico').then(response => response.json()),
+]
+
+const af = [
+    fetch('http://worldtimeapi.org/api/timezone/Africa/Algiers').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/Africa/Cairo').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/Africa/Juba').then(response => response.json()),
+    fetch('http://worldtimeapi.org/api/timezone/Africa/Bissau').then(response => response.json()),
 ]
